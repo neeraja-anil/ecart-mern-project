e2714707eec3c695c303fe7f14be36897629177d
neeraja <neerajaanilkumar001@gmail.com>
2023-04-03 19:39:15 +0530

    PlaceOrder Screen created

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -9,6 +9,22 @@ import Message from '../components/Message'
 const PlaceOrderScreen = () => {
     const cart = useSelector(state => state.cart)
 
+    //calculate Prices
+
+    const addDecimals = (num) => {
+        return (Math.round(num * 100) / 100).toFixed(2)
+    }
+
+    cart.itemsPrice = addDecimals(cart.cartItems.reduce((acc, item) =>
+        acc + item.price * item.qty, 0
+    )
+    )
+    cart.shippingPrice = addDecimals(cart.itemsPrice > 200 ? 0 : 20)
+
+    cart.taxPrice = addDecimals(Number((0.10 * cart.itemsPrice).toFixed(2)))
+    cart.totalPrice = (Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)).toFixed(2)
+
+
     const placeOrderHandler = () => {
         console.log('place Order')
     }
