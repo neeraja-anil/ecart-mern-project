1c2fd6f54dbfd90ca9359a8285f3356908ee796b
neeraja <neerajaanilkumar001@gmail.com>
2023-04-06 13:27:45 +0530

    admin view access security

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -16,7 +16,8 @@ import {
     USER_DETAILS_RESET,
     USER_LIST_REQUEST,
     USER_LIST_SUCCESS,
-    USER_LIST_FAIL
+    USER_LIST_FAIL,
+    USER_LIST_RESET
 } from '../constants/userConstants'
 
 import { ORDER_CREATE_RESET, ORDER_MY_LIST_RESET } from '../constants/orderConstants'
@@ -55,6 +56,7 @@ export const logout = () => (dispatch) => {
     dispatch({ type: USER_DETAILS_RESET })
     dispatch({ type: ORDER_MY_LIST_RESET })
     dispatch({ type: ORDER_CREATE_RESET })
+    dispatch({ type: USER_LIST_RESET })
 
 }
 
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -8,14 +8,24 @@ import Loader from '../components/Loader'
 import { listUsers } from '../actions/userActions'
 
 const UserListScreen = () => {
+
+    const navigate = useNavigate()
     const dispatch = useDispatch()
 
     const userList = useSelector(state => state.userList)
     const { loading, error, users } = userList
 
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
     useEffect(() => {
-        dispatch(listUsers())
-    }, [dispatch])
+        if (userInfo && userInfo.isAdmin) {
+            dispatch(listUsers())
+        } else {
+            navigate('/login')
+        }
+
+    }, [dispatch, navigate, userInfo])
 
     const deleteHandler = (id) => {
         console.log(id)
