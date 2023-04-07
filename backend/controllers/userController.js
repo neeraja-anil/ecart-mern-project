224830b17560da3051d3dbe695cb08621d3521a6
neeraja <neerajaanilkumar001@gmail.com>
2023-04-07 13:51:07 +0530

    edit user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -138,5 +138,47 @@ const deleteUsers = asyncHandler(async (req, res) => {
 
 })
 
+//@desc   Get user by id to update
+//@route  POST /api/users/:id
+//@access private/Admin
+const getUserById = asyncHandler(async (req, res) => {
 
-export { authUser, getUserProfile, registerNewUser, updateUserProfile, getUsers, deleteUsers }
+    const user = await User.findById(req.params.id).select('-password')
+    if (user) {
+        res.json(user)
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+})
+
+//@desc   Update user 
+//@route  PUT /api/users/:id
+//@access private/admin
+const updateUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        user.isAdmin = req.body.isAdmin
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        })
+
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+
+
+export { authUser, getUserProfile, registerNewUser, updateUserProfile, getUsers, deleteUsers, getUserById, updateUser }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { authUser, getUserProfile, registerNewUser, updateUserProfile, getUsers, deleteUsers } from "../controllers/userController.js";
+import { authUser, getUserProfile, registerNewUser, updateUserProfile, getUsers, deleteUsers, getUserById, updateUser } from "../controllers/userController.js";
 import { protect, admin } from '../middlewares/authMiddleware.js'
 const router = express.Router()
 
 router.route('/').post(registerNewUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUsers)
+router.route('/:id').delete(protect, admin, deleteUsers).get(protect, admin, getUserById).put(protect, admin, updateUser)
 
 
 export default router
