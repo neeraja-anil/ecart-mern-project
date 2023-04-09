99bbfebb05ff6e448054f9cf04617ed32ff7a14e
neeraja <neerajaanilkumar001@gmail.com>
2023-04-09 13:48:10 +0530

    Image Upload setup using multer

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 import connectDB from './config/db.js'
@@ -6,6 +7,7 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import UserRoutes from './routes/userRoutes.js'
 import OrderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -21,8 +23,12 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', UserRoutes)
 app.use('/api/orders', OrderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
