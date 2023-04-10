2a315d0996f771db4ce90973452dd5cad557cae4
neeraja <neerajaanilkumar001@gmail.com>
2023-04-10 20:36:26 +0530

    routes for creating new product reviews

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -5,7 +5,12 @@ import { Schema } from "mongoose"
 const reviewSchema = new Schema({
     name: { type: String, required: true },
     rating: { type: Number, required: true },
-    comment: { type: String, required: true }
+    comment: { type: String, required: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
 }, { timestamps: true })
 
 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getProductById, getProducts, deleteProduct, createProduct, updateProduct } from "../controllers/productController.js";
+import { getProductById, getProducts, deleteProduct, createProduct, updateProduct, createProductReview } from "../controllers/productController.js";
 import { protect, admin } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
@@ -7,5 +7,6 @@ const router = express.Router()
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 
 export default router
