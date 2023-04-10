448fd0563cedc53a715fc2d4736d66c8684ee06b
neeraja <neerajaanilkumar001@gmail.com>
2023-04-10 20:35:51 +0530

    morgan package

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 import connectDB from './config/db.js'
 
@@ -13,6 +14,10 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.use(express.json()) //body parser
 
