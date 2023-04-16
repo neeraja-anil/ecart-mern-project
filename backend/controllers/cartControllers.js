29f267dbc9fad255b208f8955bed128c17953c94
neeraja <neerajaanilkumar001@gmail.com>
2023-04-17 00:42:56 +0530

    edited cart controller

--- backend/controllers/cartControllers.js
+++ backend/controllers/cartControllers.js
@@ -8,17 +8,50 @@ import Cart from '../models/cartModel.js'
 const addCartItems = asyncHandler(async (req, res) => {
     const { cartItems } = req.body
 
-    if (cartItems && cartItems.length === 0) {
-        res.status(400)
-        throw new Error('NO ITEMS IN CART')
+    const cart = await Cart.findOne({ user: req.user._id })
+    if (cart) {
+        const item = cart.cartItems.find(x => x.product.equals(cartItems.product))
+        let condition, update
+
+        if (item) {
+            condition = { user: req.user._id, "cartItems.product": cartItems.product }
+            update = {
+                "$set": {
+                    "cartItems.$": {
+                        ...req.body.cartItems,
+                        qty: item.qty + req.body.cartItems.qty
+                    }
+                }
+            }
+        } else {
+            //res.status(200).json({ message: cartItems })
+            condition = { user: req.user._id }
+            update = {
+                "$push": {
+                    "cartItems": req.body.cartItems
+                }
+            }
+        }
+        const ifCart = await Cart.findOneAndUpdate(condition, update)
+        if (ifCart) {
+            return res.status(201).json({ cart: cart })
+        }
+
     } else {
-        const cart = new Cart({
-            cartItems,
-            user: req.user._id,
-        })
-        const createdCart = await cart.save()
-        res.status(201).json(createdCart)
+        if (cartItems && cartItems.length === 0) {
+            res.status(400)
+            throw new Error('NO ITEMS IN CART')
+        } else {
+            const cart = new Cart({
+                cartItems,
+                user: req.user._id,
+            })
+            const createdCart = await cart.save()
+            res.status(201).json(createdCart)
+        }
     }
+
+
 })
 
 //@desc GET cart items from db
